1a10ad33f084c0c23b2628a5721d13774f43fbee
PRATHAM-p25 <pratham.25102003@gmail.com>
2024-04-19 23:33:47 +0530

    Added dynamic-calendar-program

--- dynamic-calendar-program.c
+++ dynamic-calendar-program.c
@@ -1,1 +1,49 @@
+#include <stdio.h> 
+#include<stdlib.h> 
 
+typedef struct { 
+    char *day; 
+    int dt; 
+    char *desc; 
+} date; 
+
+void create(); 
+date *calender; 
+void read(); 
+void display(); 
+
+void main() { 
+    printf("Creating\n"); 
+    create(); 
+    printf("Reading\n"); 
+    read(); 
+    printf("Displaying\n"); 
+    display(); 
+} 
+
+void create() { 
+    calender=(date*)malloc(sizeof(date)*7); 
+} 
+
+void read() { 
+    int i; 
+    for(i=0;i<7;i++) { 
+        printf("\nEnter day of the week:"); 
+        calender[i].day = (char*)malloc(sizeof(char)*100); 
+        scanf("%s",calender[i].day); 
+        printf("\nEnter the date:"); 
+        scanf("%d",&calender[i].dt); 
+        printf("\nEnter the description:"); 
+        calender[i].desc = (char*)malloc(sizeof(char)*100); 
+        scanf("%s",calender[i].desc); 
+    } 
+} 
+
+void display() { 
+    int i; 
+    for(i=0;i<7;i++) { 
+        printf("\nDay of the week:%s\n",calender[i].day ); 
+        printf("\nDate:%d\n",calender[i].dt); 
+        printf("\nDescription:%s\n",calender[i].desc); 
+    } 
+} 
